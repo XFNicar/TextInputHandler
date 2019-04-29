72957245afdecc00caecda9d5a0661a2e50bea12
XieFei <xuejingwen25@163.com>
2019-04-29 22:45:41 +0800

    添加项目

---
+++ TextInputHandler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextInputHandler
+//
+//  Created by YanYi on 2019/4/29.
+//  Copyright © 2019 YanYi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextInputHandler/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TextInputHandler
+//
+//  Created by YanYi on 2019/4/29.
+//  Copyright © 2019 YanYi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
